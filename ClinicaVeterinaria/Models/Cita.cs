308b73be44f972d4e2448b365739f7074be80d96
daniel santiago de la hoz coha <zafitodjks@gmail.com>
2026-08-11 11:01:59 -0500

    Estoy creando el models Cita y conectandolo con lo demas, ademas empee a usar herencia

--- ClinicaVeterinaria/Models/Cita.cs
+++ ClinicaVeterinaria/Models/Cita.cs
@@ -1,6 +1,14 @@
 ﻿namespace ClinicaVeterinaria.models;
 
-public class Cita
+public class Cita : Mascota
 {
-    
+        public Guid Id { get; set; }
+        public DateTime FechaCita { get; set; }
+        public string MotivoCita { get; set; }
+        public enum EstadoCita
+        {
+               Pendiente = 1,
+               Confirmada = 2,
+               Cancelado = 3
+        }
 }
---
+++ ClinicaVeterinaria/Models/Mascota.cs
@@ -0,0 +1,13 @@
+﻿namespace ClinicaVeterinaria.models;
+
+public class Mascota
+{
+    public Guid Id { get; set; }
+    public string Nombre { get; set; }
+    public int Edad { get; set; }
+    public int Peso { get; set; }
+    public string? Raza { get; set; }
+    public Guid? DuenoId { get; set; }
+    
+    
+}
